0887d1be4f6901075b1fd71c496a8dbb0fec0f69
Shola Ade <sadey2k@yahoo.com>
2022-09-02 00:50:53 +0100

    update4

---
+++ release-pipeline.sh
@@ -0,0 +1,45 @@
+#####################################
+## Step-01: Create Namespaces ##
+#####################################
+kubectl create ns dev
+kubectl create ns staging
+kubectl create ns prod
+
+##########################################
+## Step-02: Create Service Connections ##
+##########################################
+23-dev-ns-aks-svc
+23-qa-ns-aks-svc
+23-prod-ns-aks-svc
+
+##########################################
+## Step-03: Creat  Release Pipelines ##
+##########################################
+Empty jpb
+Add Artifact 
+Enabe continious deployment 
+
+Agent Job
+     - 
+
+Add Tasks
+    - Deploy to Kubernetes
+        - Name = Create Secrete to allow image pull from ACR
+        - Action = create secret
+        - Kubernetes Service connection = 23-dev-ns-aks-svc
+        - Namespace = dev
+        - type of secret - dockerRegistry
+        - Docker registry service connection = manual-aksdevops-acradedemo2 # one create for build pipeline
+
+    - Deploy to Kubernetes
+        - Name = Deploy to AKS
+        - Action = deploy
+        - Kubernetes Service connection = 23-dev-ns-aks-svc
+        - Namespace = dev
+        - Manifests = manifest location 
+        - Containers = acradedemo2.azurecr.io/custom2aksnginxapp2 # container created during Docker build and push to ACR
+        - Image Pull Secrets = dev-acradedemo2-secret # created during create secret task
+
+    - Change pipeline name to = 01-app1-release-pipeline
+
+    
